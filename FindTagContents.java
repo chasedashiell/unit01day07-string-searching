6444d00c2cd9954fda3a55f1ea29660bbd50c260
chasedashiell <dashchax@gmail.com>
2023-01-27 12:29:38 -0600

    not done

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
+        //gets input from user.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the HTML you would like to search: ");
+        String HTML = scanner.nextLine();
+        System.out.println("Please enter the tag types contents you would like to search for (ex. b): ");
+        String tag = scanner.nextLine();
+
+        //finds first instance of the tag and prints where it was found.
+        int firstIndexOfTag = HTML.indexOf(tag);
+        System.out.println("Found " + tag + " at index " + firstIndexOfTag);
+
+        int endIndexOfTag = HTML.indexOf("/" + tag);
+        //finds the rough area
+        String sub = HTML.substring(firstIndexOfTag, endIndexOfTag);
+
+        // finishes it off
+        String answer = sub.substring(sub.indexOf(">")+1, sub.indexOf("<"));
+        System.out.println(answer);
+
         
     }
 }
--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class FindWord {
     public static void main(String[] args) {
-        
+        // user input
+        // find last index String.lastIndexOf();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the sentence that you would like to search: ");
+        String sentence = scanner.nextLine();
+        String newSentence = " " + sentence + " ";
+        System.out.println("Please enter the letter you would like to find the first word its in: ");
+        String letter = scanner.nextLine();
+        int indexOfLetter = newSentence.indexOf(letter);
+        String letterToEnd = newSentence.substring(indexOfLetter);
+        int nextIndexOf = letterToEnd.indexOf(" ");
+        String begin = newSentence.substring(0, indexOfLetter);
+        int indexOfFirstLetter = begin.lastIndexOf(" ");
+
+        String beginWord = newSentence.substring(indexOfFirstLetter, indexOfLetter);
+
+        String endWord = newSentence.substring(indexOfLetter, nextIndexOf);
+
+
+        System.out.println(beginWord + endWord);
     }
 }
